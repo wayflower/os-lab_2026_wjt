41673b27016d227430fdf7713f9aae5c7fa087cc
pku2300013097 <2300013097@stu.pku.edu.cn>
2026-03-11 13:15:01 +0000

    copy my kernel code

---
+++ xv6-k210/kernel/include/sysnum.h
@@ -0,0 +1,33 @@
+#ifndef __SYSNUM_H
+#define __SYSNUM_H
+
+// System call numbers
+#define SYS_fork 1
+#define SYS_exit 93
+#define SYS_wait 260
+#define SYS_pipe 59
+#define SYS_read 63
+#define SYS_kill 6
+#define SYS_exec 221
+#define SYS_fstat 80
+#define SYS_chdir 49
+#define SYS_dup 23
+#define SYS_getpid 172
+#define SYS_sbrk 214
+#define SYS_sleep 101
+#define SYS_uptime 14
+#define SYS_open 56
+#define SYS_write 64
+#define SYS_remove 17
+#define SYS_trace 18
+#define SYS_sysinfo 19
+#define SYS_mkdir 34
+#define SYS_close 57
+#define SYS_test_proc 22
+#define SYS_dev 23
+#define SYS_readdir 24
+#define SYS_getcwd 17
+#define SYS_rename 26
+#define SYS_shutdown 10001
+
+#endif
